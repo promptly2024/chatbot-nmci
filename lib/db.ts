ec19405d9a27b44cc3576b63ec466dc5c0d71811
Samarth Rajput <ssamarth224@gmail.com>
2025-08-29 16:39:43 +0530

    init

---
+++ lib/db.ts
@@ -0,0 +1,20 @@
+// First we create schema, then we migrate your db, and then we create clients 
+// We have used singleton because when we do hot reload client also regenerates
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+// eslint-disable-next-line
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
