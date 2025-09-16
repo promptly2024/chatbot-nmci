f047c256129a4788653a465589a4a4e5249fdfde
Rohit kumar yadav <rohitkuyada@gmail.com>
2025-09-16 15:16:48 +0530

    fix: re-enable eslint rules for unused vars and img elements in MarkdownRenderer

--- components/MarkdownRenderer.tsx
+++ components/MarkdownRenderer.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from 'remark-gfm';
